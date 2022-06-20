a1e44c1829f2ccfb02c24640c23a882e89fa519c
vinayredkar <86841694+vinayredkar@users.noreply.github.com>
2022-06-20 16:39:16 +0530

    set operation in union, intersection, etc

---
+++ SetOperation.py
@@ -0,0 +1,10 @@
+E = {0, 2, 4, 6, 8};
+N = {1, 2, 3, 4, 5};
+#UNION
+print("Union between E & N",E|N)
+#INTERSECTION
+print("Intersecton Between E & N",E&N)
+#DIFFERENCE IN E & N
+print("Diffference between e & n",E-N)
+#SYMMETRIC DIFFERENCE BETWEEN E & N
+print("Symmetric difference",E^N)
